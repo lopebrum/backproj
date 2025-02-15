fe6865017b57fdcf76774e21c9615e5073abeb35
lopebrum <nurakhmetkaliyev@gmail.com>
2025-02-15 19:18:07 +0500

    Added two beans

--- src/main/java/org/example/ProjectConfig.java
+++ src/main/java/org/example/ProjectConfig.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,5 +8,14 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "org.example" )
 public class ProjectConfig {
 
+    @Bean
+    public Destination parisDestination() {
+        return new Destination("Paris", 500.0);
+    }
+
+    @Bean
+    public Booking AliceBooking(Destination destination) {
+        return new Booking("Alice", destination);
+    }
 
 }
